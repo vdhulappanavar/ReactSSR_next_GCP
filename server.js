340629a929839a64306fb19a7e71eead765d9a09
vdhulappanavar@sapient.com <vdhulappanavar@sapient.com>
2019-04-25 09:28:48 +0530

    check simple node app deployment

---
+++ server.js
@@ -0,0 +1,37 @@
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const errorHandler = require('_helpers/error-handler');
+const path = require('path')
+const csp = require('helmet-csp')
+/*
+app.use(csp({
+    directives: {
+    defaultSrc: ["'self'", 'data:font'],
+    fontSrc: ["'self'", 'data:font']
+    }
+}))
+*/
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/mockCheck', (req, res) => {
+    res.send({msg: 'hello'})
+})
+
+app.post('/mockCheckPost', function (req, res) {
+    console.log(req.body)
+    res.send({"checkmsg": "checkMsg"})
+
+  })
+// global error handler
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
